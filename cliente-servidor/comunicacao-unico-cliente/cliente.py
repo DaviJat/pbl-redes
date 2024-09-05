56c0ecddb938d3b5d417452dcd6fb5e5dc748b30
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-09-05 17:13:41 -0300

    Comentando código

--- cliente-servidor/comunicacao-unico-cliente/cliente.py
+++ cliente-servidor/comunicacao-unico-cliente/cliente.py
@@ -1,27 +1,33 @@
-import socket
+import socket # Importa o módulo socket para permitir a comunicação em rede
 
 while True:
+        # Solicita ao usuário o endereço IP do servidor e a porta para a conexão
         print("----- Iniciar nova conexão -----")
-        ip_servidor = input("Endereço do servidor: ")
+        ip_servidor = input("IP do servidor: ")
         porta_servidor = input("Porta do servidor: ")
 
+        # Cria um socket cliente usando o protocolo IPv4 (AF_INET) e TCP (SOCK_STREAM)
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((ip_servidor, int(porta_servidor)))
 
+        # Inicia conexão com servidor do endereço fornecido
+        client_socket.connect((ip_servidor, int(porta_servidor)))
         print("----- Conexão estabelecida -----")
 
+        # Loop com a troca de mensagens entre cliente e servidor
         print("\n----- Chat com o servidor -----")
-
         while True:
-            message_to_send = input("Mensagem para o servidor: ")
 
+            # Envia resposta digitada para o servidor codificada em formato UTF-8
+            message_to_send = input("Mensagem para o servidor: ")
             client_socket.send(message_to_send.encode("utf-8"))
 
+            # Se a mensagem enviada for "exit", encerra a conexão    
             if message_to_send == 'exit':
                 client_socket.close()    
                 print('\n----- Conexão encerrada -----')
                 break
 
+            # Recebe a resposta do servidor, até 248 bytes, e decodifica de UTF-8 para string.    
             received_message = client_socket.recv(248).decode("utf-8")
             print("Mensagem do servidor: " + received_message)
         
--- cliente-servidor/comunicacao-unico-cliente/servidor.py
+++ cliente-servidor/comunicacao-unico-cliente/servidor.py
@@ -1,39 +1,49 @@
-import socket
-import random
+import socket # Importa o módulo socket para permitir a comunicação em rede
 
-SERVER_PORT = random.randint(15000, 65535)
+# Define a porta do servidor como 3000
+SERVER_PORT = 3000 
+# Obtém o endereço IP do servidor, pegando o IP da máquina local.
 SERVER_IP = socket.gethostbyname(socket.gethostname())
 
-counter_received = 0
-counter_sent = 0
-
-NUM_BYTES_PACKAGES_RECEIVED = 248
-
 while True:
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((SERVER_IP, SERVER_PORT))
+    # Cria um socket servidor usando o protocolo IPv4 (AF_INET) e TCP (SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Associa o IP local e a porta fornecida ao socket do servidor
+    server_socket.bind((SERVER_IP, SERVER_PORT))
+
+    # Exibe o IP e a porta do servidor
     print("IP do servidor: " + SERVER_IP)
     print("Porta do servidor: " + str(SERVER_PORT))
 
-    server.listen(5)
-
+    # Coloca o servidor em modo de escuta, esperando por conexões (máximo de 1 conexão)
+    server_socket.listen(1)
     print("Esperando por cliente: \n")
 
-    socketConnection, address = server.accept()
+    # Aceita a conexão com o cliente e captura o socket da conexão e o endereço do cliente
+    socketConnection, address = server_socket.accept()
     print("Servidor estabeleceu conexão com o cliente: " + str(address) + "\n")
-    print(("-"*5) + " Chat com o cliente " + ("-"*5))
+    print("----- Chat com o cliente -----")
 
+    # Loop com a troca de mensagens entre cliente e servidor
     while True:
-        msg_bytes = socketConnection.recv(1024)
-        clientHost, clientPort = socketConnection.getpeername()
-        msg_received_str = msg_bytes.decode("utf-8")
+        # Recebe a mensagem enviada do cliente (limitando a 248 bytes)
+        msg_received_bytes = socketConnection.recv(248)
 
+        # Decodifica os bytes recebidos para string
+        msg_received_str = msg_received_bytes.decode("utf-8")
+
+        # Encerra conexão se o cliente enviar a mensagem "exit"
         if msg_received_str != "exit":
+            print("Cliente saiu do chat")
+            print("----- Conexão Encerrada -----" + "\n\n")
+            break
+        else:
+            # Mostra a mensagem recebida do cliente
             print("Mensagem do cliente: " + str(msg_received_str))
+            
+            # Envia resposta digitada para o cliente codificada em formato UTF-8
             msg = input("Mensagem para o cliente: ")
             socketConnection.send(msg.encode("utf-8"))
-        else:
-            print("Cliente saiu do chat")
-            print(("-"*5) + " Conexão Encerrada " + ("-"*5) + "\n\n")
-            break;
+            
     
