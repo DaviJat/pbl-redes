fee228010869753e108721d9f205eaf264b1b991
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-09-11 19:17:06 -0300

    Retornando index.html

--- cliente-servidor/comunicacao-stateless/servidor.py
+++ cliente-servidor/comunicacao-stateless/servidor.py
@@ -1,50 +1,39 @@
 import socket
- 
-HOST,PORT = '127.0.0.1',8082
- 
-my_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-my_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-my_socket.bind((HOST,PORT))
+import os
+
+# Configurações básicas
+SERVER_IP = socket.gethostbyname(socket.gethostname())
+SERVER_PORT = 3000 
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.bind((SERVER_IP, SERVER_PORT))
 my_socket.listen(1)
- 
-print('Serving on port ',PORT)
- 
+
+print(SERVER_IP, SERVER_PORT)
+
 while True:
-    connection,address = my_socket.accept()
+    connection, address = my_socket.accept()
     request = connection.recv(1024).decode('utf-8')
-    string_list = request.split(' ')     # Split request from spaces
- 
-    method = string_list[0]
-    requesting_file = string_list[1]
- 
-    print('Client request ',requesting_file)
- 
-    myfile = requesting_file.split('?')[0] # After the "?" symbol not relevent here
-    myfile = myfile.lstrip('/')
-    if(myfile == ''):
-        myfile = 'index.html'    # Load index file as default
- 
     try:
-        file = open(myfile,'rb') # open file , r => read , b => byte format
-        response = file.read()
-        file.close()
- 
-        header = 'HTTP/1.1 200 OK\n'
- 
-        if(myfile.endswith(".jpg")):
-            mimetype = 'image/jpg'
-        elif(myfile.endswith(".css")):
-            mimetype = 'text/css'
-        else:
-            mimetype = 'text/html'
- 
-        header += 'Content-Type: '+str(mimetype)+'\n\n'
- 
+        # Tentando abrir e ler o arquivo HTML
+        file_path = os.path.join(os.path.dirname(__file__), 'index.html')
+        print(file_path)
+
+        with open(file_path, 'r') as file:
+            response = file.read()
+
+        # Cabeçalho de sucesso
+        header = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'
+        # Transforma o conteúdo do HTML em bytes
+        response = response.encode('utf-8')
+
     except Exception as e:
+        print(e)
         header = 'HTTP/1.1 404 Not Found\n\n'
-        response = '<html><body><center><h3>Error 404: File not found</h3><p>Python HTTP Server</p></center></body></html>'.encode('utf-8')
- 
-    final_response = header.encode('utf-8')
-    final_response += response
+        response = '<h1>404 Not Found</h1>'.encode('utf-8')
+
+    # Envia a resposta
+    final_response = header.encode('utf-8') + response
     connection.send(final_response)
     connection.close()
